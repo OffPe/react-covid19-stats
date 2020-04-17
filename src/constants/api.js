98be8d0f75706ab920b832fb760c5ab169a83cba
jaideepghosh <er.jaideepghosh@gmail.com>
2020-04-17 16:26:23 +0530

    added api consts.

---
+++ src/constants/api.js
@@ -0,0 +1,14 @@
+const API_ENDPOINTS = {
+  DATA: "https://api.covid19india.org/data.json",
+  STATE_DISTRICT_WISE: "https://api.covid19india.org/state_district_wise.json",
+  STATE_DISTRICT_WISE_V2:
+    "https://api.covid19india.org/v2/state_district_wise.json",
+  TRAVEL_HISTORY: "https://api.covid19india.org/travel_history.json",
+  RAW_DATA: "https://api.covid19india.org/raw_data.json",
+  STATES_DAILY_CHANGES: "https://api.covid19india.org/states_daily.json",
+  DEATHS_AND_RECOVERIES: "https://api.covid19india.org/deaths_recoveries.json",
+  ESSENTIALS_AND_RESOURCES:
+    "https://api.covid19india.org/resources/resources.json"
+};
+
+export default API_ENDPOINTS;
