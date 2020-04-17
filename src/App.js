be8906f821dcb0156235201c694a5d5632f3c425
jaideepghosh <er.jaideepghosh@gmail.com>
2020-04-17 20:03:47 +0530

    added feat to dynamically get the data.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Link from "@material-ui/core/Link";
 import Grid from "@material-ui/core/Grid";
@@ -7,6 +7,9 @@ import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
 import StatGrid from "./components/StatGrid";
+import axios from "axios";
+import moment from "moment";
+import API_ENDPOINTS from "./constants/api";
 
 function Copyright() {
   return (
@@ -28,6 +31,9 @@ const useStyles = makeStyles((theme) => ({
     flexDirection: "column",
     alignItems: "center"
   },
+  row: {
+    marginBottom: theme.spacing(3)
+  },
   primaryText: {
     color: theme.palette.primary.main
   },
@@ -42,42 +48,114 @@ const useStyles = makeStyles((theme) => ({
   }
 }));
 
+const numberWithCommas = (count) => {
+  return count.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+};
+
 export default function App() {
+  const [appState, setAppState] = useState({
+    meta: {
+      confirmed_total_count: 0,
+      active_total_count: 0,
+      recovered_total_count: 0,
+      deceased_total_count: 0,
+      confirmed_latest_total_count: 0,
+      active_latest_total_count: 0,
+      recovered_latest_total_count: 0,
+      deceased_latest_total_count: 0,
+      last_updated: ""
+    }
+  });
+
+  const setData = async () => {
+    const api_response = await axios.get(API_ENDPOINTS.DATA);
+
+    setAppState({
+      meta: {
+        confirmed_total_count: numberWithCommas(
+          api_response.data.statewise[0].confirmed
+        ),
+        active_total_count: numberWithCommas(
+          api_response.data.statewise[0].active
+        ),
+        recovered_total_count: numberWithCommas(
+          api_response.data.statewise[0].recovered
+        ),
+        deceased_total_count: numberWithCommas(
+          api_response.data.statewise[0].deaths
+        ),
+        confirmed_latest_total_count: 0,
+        active_latest_total_count: 0,
+        recovered_latest_total_count: 0,
+        deceased_latest_total_count: 0,
+        last_updated: moment(
+          api_response.data.statewise[0].lastupdatedtime,
+          "DD/MM/YYYY hh:mm:ss"
+        ).fromNow()
+      }
+    });
+  };
+
+  useEffect(() => {
+    setData(appState);
+  }, [appState]);
+
   const classes = useStyles();
 
   return (
     <Container component="main">
       <CssBaseline />
       <div className={classes.paper}>
+        <Grid className={classes.row} container justify="space-between">
+          <Grid className={classes.statsItem} item>
+            <Typography display="inline" variant="h4" color="primary">
+              Covid19 Tracker
+              <Typography display="inline" variant="caption" color="primary">
+                (India)
+              </Typography>
+            </Typography>
+          </Grid>
+          <Grid className={classes.statsItem} item>
+            <Typography display="inline" variant="caption" color="primary">
+              Last updated
+              <br />
+            </Typography>
+            <Typography display="inline" variant="subtitle2" color="primary">
+              {appState.meta.last_updated}
+            </Typography>
+          </Grid>
+        </Grid>
+
         <Grid container spacing={4}>
           <Grid item lg={3} sm={6} xl={3} xs={12}>
             <StatGrid
               stattext="Confirmed"
-              totalcount="13,031"
-              latestcount="+660"
+              totalcount={appState.meta.confirmed_total_count}
+              latestcount={appState.meta.confirmed_latest_total_count}
               className={classes.errorText}
             />
           </Grid>
           <Grid item lg={3} sm={6} xl={3} xs={12}>
             <StatGrid
               stattext="Active"
-              totalcount="13,031"
+              totalcount={appState.meta.active_total_count}
+              latestcount={appState.meta.active_latest_total_count}
               className={classes.primaryText}
             />
           </Grid>
           <Grid item lg={3} sm={6} xl={3} xs={12}>
             <StatGrid
               stattext="Recovered"
-              totalcount="13,031"
-              latestcount="+660"
+              totalcount={appState.meta.recovered_total_count}
+              latestcount={appState.meta.recovered_latest_total_count}
               className={classes.successText}
             />
           </Grid>
           <Grid item lg={3} sm={6} xl={3} xs={12}>
             <StatGrid
               stattext="Deceased"
-              totalcount="13,031"
-              latestcount="+660"
+              totalcount={appState.meta.deceased_total_count}
+              latestcount={appState.meta.deceased_latest_total_count}
               className={classes.greyText}
             />
           </Grid>
