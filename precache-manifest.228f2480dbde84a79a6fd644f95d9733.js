dc25c254e6fee9317d0637047e50ec6eb26be170
jaideepghosh <er.jaideepghosh@gmail.com>
2020-11-09 11:31:55 +0530

    Updates

---
+++ precache-manifest.228f2480dbde84a79a6fd644f95d9733.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80848245883c5c2ea013546dcd468a9d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aa5e78d33455bd3779c1",
+    "url": "/static/css/main.d4cfcf04.chunk.css"
+  },
+  {
+    "revision": "339f6fdceb282e8b26ab",
+    "url": "/static/js/2.189b55e2.chunk.js"
+  },
+  {
+    "revision": "68ba00d5eb083746d913686923a3d904",
+    "url": "/static/js/2.189b55e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa5e78d33455bd3779c1",
+    "url": "/static/js/main.964d73f1.chunk.js"
+  },
+  {
+    "revision": "3f1714c09c8765248c99",
+    "url": "/static/js/runtime-main.a47df422.js"
+  }
+]);
